800f0535ca5f3ff37f9bbde91e309d1a4c4b0641
hiroshimashu <hiroshimashu@gmail.com>
2019-02-12 23:19:44 +0900

    add facebook authentication using firebase

--- Machi-Ceirge-ios/ViewController.swift
+++ Machi-Ceirge-ios/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // ログイン済みかチェック
+        if let token = FBSDKAccessToken.current() {
+            let credential = FacebookAuthProvider.credential(withAccessToken: token.tokenString)
+            Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+                if error != nil {
+                    // ...
+                    return
+                }
+                // ログイン時の処理
+            }
+            return
+        }
+        // ログインボタン設置
+        let fbLoginBtn = FBSDKLoginButton()
+        fbLoginBtn.readPermissions = ["public_profile", "email"]
+        fbLoginBtn.center = self.view.center
+        fbLoginBtn.delegate = self
+        self.view.addSubview(fbLoginBtn)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // login callback
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        if error != nil {
+            print("Error")
+            return
+        }
+        // ログイン時の処理
+    }
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+    // Logout callback
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+    }
 }
 
--- Podfile
+++ Podfile
@@ -1,14 +1,14 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+# platform :ios, '9.0'
 
 target 'Machi-Ceirge-ios' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Machi-Ceirge-ios
-pod 'Firebase/Core'
-pod 'Firebase/Auth'
-pod 'FBSDKLoginKit'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'FBSDKLoginKit'
 
   target 'Machi-Ceirge-iosTests' do
     inherit! :search_paths
